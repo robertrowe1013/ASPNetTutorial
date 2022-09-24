4c92c5af1ac15354d75d2e017422de92e5336728
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2022-09-24 00:09:30 -0500

    add database context, remove swagger calls

--- APITutorial/APITutorial/Program.cs
+++ APITutorial/APITutorial/Program.cs
@@ -1,19 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using APITutorial.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
+//});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
